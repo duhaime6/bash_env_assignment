4c368f815676026fc29d12c8d18d5d25a9400b15
duhaime6 <duhaime.6@buckeyemail.osu.edu>
2025-06-23 12:53:55 -0400

    Update and rename hello.sh to hello_bash.sh

---
+++ hello_bash.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# coding: utf-8
+
+# # Hello World in Bash
+# This notebook demonstrates a simple "Hello, World!" script in Bash.
+
+# In[1]:
+
+
+#!/bin/bash
+echo "Hello, World from Bash!"
+
+
+# In[ ]:
+
+
+
+
