435d9e8981792e74c7d9605d8fde0f2f754efc77
duhaime6 <duhaime.6@buckeyemail.osu.edu>
2025-06-23 12:52:03 -0400

    Update and rename hello.py to hello_python.py

---
+++ hello_python.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Hello World in Python
+# This notebook demonstrates a simple "Hello, World!" script in Python.
+
+# In[1]:
+
+
+print("Hello, World from Python!")
+
+
+# In[ ]:
+
+
+
+
